a6f314bca4a363ea1ddd1276684d5a3cb488a3a9
toimc <brian@toimc.com>
2021-07-11 15:47:50 +0800

    feat: [main] 添加element-plus

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <el-button type="primary">打开</el-button>
 </template>
 
 <script lang="ts">
   import { defineComponent } from 'vue'
-  import HelloWorld from './components/HelloWorld.vue'
   export default defineComponent({
     name: 'App',
-    components: {
-      HelloWorld
-    }
+    components: {}
   })
 </script>
 
-<style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
+<style lang="scss">
+  @import 'element-plus/packages/theme-chalk/src/base.scss';
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import element from './plugins/element'
+
+const app = createApp(App)
+
+app.use(element)
+app.mount('#app')
