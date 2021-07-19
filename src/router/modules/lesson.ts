8b3520d13be8f36bf233fd4865b7ea1302b2bdd7
wcd123456 <825342538@qq.com>
2021-07-19 16:34:54 +0800

    feat(每日一课): 新增每日一课切图，安装了tinymce依赖，public文件夹下引入tinymce相关文件

--- src/router/modules/lesson.ts
+++ src/router/modules/lesson.ts
@@ -2,6 +2,7 @@ import content from '@/views/content-manage/lesson/content.vue'
 import audit from '@/views/content-manage/lesson/audit.vue'
 import comment from '@/views/content-manage/lesson/comment.vue'
 import learning from '@/views/content-manage/lesson/learning.vue'
+import add from '@/views/content-manage/lesson/add.vue'
 const lesson = [
   {
     path: '/content-manage/lesson/content',
@@ -18,6 +19,10 @@ const lesson = [
   {
     path: '/content-manage/lesson/learning',
     component: learning
+  },
+  {
+    path: '/content-manage/lesson/add',
+    component: add
   }
 ]
 
